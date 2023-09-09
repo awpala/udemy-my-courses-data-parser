1a8740162457e9c2054bf4df86b53d52baba1af3
awpala <apala7@gatech.edu>
2023-09-09 05:15:17 +0000

    fix: update script

--- truncate_tables.sql
+++ truncate_tables.sql
@@ -1,12 +1,13 @@
 -- Truncate entity tables
-TRUNCATE TABLE Student.List CASCADE;
 TRUNCATE TABLE Student.Course CASCADE;
+TRUNCATE TABLE Student.List CASCADE;
 TRUNCATE TABLE Student.Instructor CASCADE;
 TRUNCATE TABLE Student.Category CASCADE;
 TRUNCATE TABLE Student.Subcategory CASCADE;
 TRUNCATE TABLE Student.Topic CASCADE;
 
 -- Truncate join tables
+TRUNCATE TABLE Student.Course_List CASCADE;
 TRUNCATE TABLE Student.Course_Instructor CASCADE;
 TRUNCATE TABLE Student.Course_Category CASCADE;
 TRUNCATE TABLE Student.Course_Subcategory CASCADE;
